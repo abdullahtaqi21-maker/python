7cd7fa8ee87b2b614e093b4128bceeb2666638cb
abdullahtaqi21-maker <abdullahtaqi21@gmail.com>
2025-10-28 21:33:15 +0530

    modules

--- learning/modules/abdutils.py
+++ learning/modules/abdutils.py
@@ -9,3 +9,7 @@ def subtract(x, y):
 
 def square(x):
     return x * x
+
+def print_table(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
--- learning/modules/mod3.py
+++ learning/modules/mod3.py
@@ -1,8 +1,19 @@
 import abdutils
 
+print("-" * 25)
+
 name = input("Enter your name:")
 n = abdutils.greet(name)
+
+print("-" * 25)
 
 x = int(input("Enter a number:"))
 sq = abdutils.square(x)
 print(f"Square of {x} is {sq}")
+
+print("-" * 25)
+
+n2 = int(input("Enter a number:"))
+n1 = abdutils.print_table(n2)
+
+print("-" * 25)
